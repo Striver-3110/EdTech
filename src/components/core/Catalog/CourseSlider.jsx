40e4796e3c70d9d903b5a4c762e289e90122ccf2
Striver-3110 <21csjay067@ldce.ac.in>
2024-05-15 09:48:41 +0530

    Implementation done for  CourseSlider component on catalog page

--- src/components/core/Catalog/CourseSlider.jsx
+++ src/components/core/Catalog/CourseSlider.jsx
@@ -1,43 +1,40 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { FreeMode, Pagination } from "swiper"
-import "swiper/css"
-import "swiper/css/free-mode"
-import "swiper/css/pagination"
+import { FreeMode, Pagination } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/pagination'
 
-export default function CourseSlider({courses}) {
-    console.log("courses length in courseSlider:.........",courses)
+export default function CourseSlider ({ courses }) {
+  console.log('courses length in courseSlider:.........', courses)
   return (
     <>
-    {
-        courses?.length ? 
-        (
-
-            <Swiper
-            slidesPerView={1}
-            spaceBetween={25}
-            loop={true}
-            modules={[FreeMode, Pagination]}
-            breakpoints={{
-              1024: {
-                slidesPerView: 3,
-              },
-            }}
-            className="max-h-[30rem]"
-            >
-                {
-                    courses.map((course,i)=>{
-                        return <SwiperSlide key={i}>
-                            {/* TODO:! course Card to be implemented*/}
-                            {/* img for testing purpose */}
-                            <img src={course?.thumbnail} alt="" />
-                        </SwiperSlide>
-                    })
-                }
-            </Swiper>
-        ): <p className="text-xl text-richblack-5">No Course Found</p>
-    }
-
+      {courses?.length ? (
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={25}
+          loop={true}
+          modules={[FreeMode, Pagination]}
+          breakpoints={{
+            1024: {
+              slidesPerView: 3
+            }
+          }}
+          className='max-h-[30rem]'
+        >
+          {courses.map((course, i) => {
+            return (
+              <SwiperSlide key={i}>
+                {/* TODO:! course Card to be implemented*/}
+                {/* img for testing purpose */}
+                <img src={course?.thumbnail} alt='' />
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      ) : (
+        <p className='text-xl text-richblack-5'>No Course Found</p>
+      )}
     </>
   )
 }
